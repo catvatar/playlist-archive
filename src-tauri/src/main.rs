ce43cbd0a14ef7c77e9df0715bc7264608f86103
catvatar <olekgogol@protonmail.com>
2024-02-24 15:56:17 +0100

    added error handling on get_video_from_youtube_by_id

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
 
 use tauri::AppHandle;
 use reqwest;
-use youtube_api::YoutubeApi;
+// use youtube_api::YoutubeApi;
 
 fn main() {
   tauri::Builder::default()
@@ -16,45 +16,36 @@ fn main() {
     .expect("error while running tauri application");
 }
 
-fn youtube_api_key(app_data_dir : std::path::PathBuf) -> String {
+fn youtube_api_key(app_data_dir : std::path::PathBuf) -> Result<String, std::io::Error> {
   let api_key_dir: std::path::PathBuf = app_data_dir.join("api_key.txt");
-  std::fs::read_to_string(api_key_dir).unwrap()
+  std::fs::read_to_string(api_key_dir)
 }
-
-// fn youtube_client_id(app_data_dir : std::path::PathBuf) -> String {
-//   let client_id_dir: std::path::PathBuf = app_data_dir.join("client_id.txt");
-//   std::fs::read_to_string(client_id_dir).unwrap()
-// }
-// fn youtube_client_secret(app_data_dir : std::path::PathBuf) -> String {
-//   let client_secret_dir: std::path::PathBuf = app_data_dir.join("client_secret.txt");
-//   std::fs::read_to_string(client_secret_dir).unwrap()
-// }
 
 async fn list_video_by_id(api_key: String, id: String) -> Result<reqwest::Response,reqwest::Error> {
   let url: &String = &format!("https://www.googleapis.com/youtube/v3/videos?part=snippet&id={}&key={}", id, api_key);
-  // let video = reqwest::get(url).await?.text().await?;
-  // println!("{}", video);
-  match reqwest::get(url).await {
-    Ok(response) => Ok(response),
-    Err(e) => Err(e)
-  }
+  reqwest::get(url).await
 }
 
 #[tauri::command]
 async fn get_video_from_youtube_by_id(app: AppHandle,id: String) -> Result<String,String>{
   let app_data_dir: std::path::PathBuf = app.path_resolver().app_data_dir().unwrap();
-  let api_key = youtube_api_key(app_data_dir.clone());
-  let result = list_video_by_id(api_key, id).await;
-  println!("{:?}", result);
-  match result {
+  
+  let api_key_result: Result<String, std::io::Error> = youtube_api_key(app_data_dir.clone());
+  let api_key: String = match api_key_result {
+    Ok(key) => key,
+    Err(e) => {
+      return Err(e.to_string());
+    }
+  };
+
+  let list_video_result:  Result<reqwest::Response,reqwest::Error> = list_video_by_id(api_key, id).await;
+  match list_video_result {
     Ok(response) => {
-      let video = response.text().await.unwrap();
+      let video: String = response.text().await.unwrap();
       Ok(video)
     },
     Err(e) => {
-      println!("{:?}", e);
-      Err("This failed".into())
+      Err(e.to_string())
     }
   }
 }
-
